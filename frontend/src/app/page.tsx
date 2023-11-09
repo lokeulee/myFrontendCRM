8585d50b1a4a5c559d9201befc8700b7329b126d
lokeulee <lokeulee@gmail.com>
2023-11-09 20:23:15 +0800

    chore: Testing the workings of the localStorage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,95 +1,38 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useEffect } from 'react';
+import { Client } from '@/types/client';
+'use client'
 
-export default function Home() {
+export default function Home(){
+
+  const client : Client = {
+    clientID: 1,
+    contactNum: 1234567890,
+    name: 'A name',
+    avatar: 'https://example.com/avatar.png',
+    organization: 'blah',
+    assignedUser: 'Another name',
+  }
+
+  const myJSON = JSON.stringify(client);
+  localStorage.setItem("1", myJSON);
+
+  console.log(localStorage);
+
+  // localStorage.setItem('username', 'Anisha');
+  // localStorage.setItem('userId', '12345');
+  if (typeof localStorage !== 'undefined') {
+    localStorage.setItem('key', 'value');
+  } else if (typeof sessionStorage !== 'undefined') {
+    // Fallback to sessionStorage if localStorage is not supported
+    sessionStorage.setItem('key', 'value');
+  } else {
+    // If neither localStorage nor sessionStorage is supported
+    console.log('Web Storage is not supported in this environment.');
+  }
+  console.log(localStorage);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      "hello world\n"
   )
 }
---
+++ frontend/src/types/client.ts
@@ -0,0 +1,8 @@
+export interface Client {
+    clientID: number;
+    contactNum: number;
+    name: string;
+    avatar: string;
+    organization: string;
+    assignedUser: string;
+}
