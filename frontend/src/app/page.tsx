2766bfefe8260c78dd81b5a8ecffb3a5e7d231e8
lokeulee <lokeulee@gmail.com>
2023-11-10 13:47:02 +0800

    chore : fix dependency issues

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,17 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import { useEffect } from 'react';
-import { Client } from '@/types/client';
 'use client'
+import { Client } from '@/types/client';
+import { Avatar, Box, Grid, List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import { Signal, useSignal } from '@preact/signals-react';
 
-export default function Home(){
+export default function Home() {
+
+  let signal = useSignal("hello");
+
+  signal.value = "derp";
+
+  console.log(signal);
+
+  console.log(signal);
 
   const client : Client = {
     clientID: 1,
@@ -15,23 +22,26 @@ export default function Home(){
     assignedUser: 'Another name',
   }
   const myJSON = JSON.stringify(client);
-  localStorage.setItem("1", myJSON);
+  // localStorage.setItem("1", myJSON);
 
-  console.log(localStorage);
+  // console.log(localStorage);
 
-  // localStorage.setItem('username', 'Anisha');
-  // localStorage.setItem('userId', '12345');
-  if (typeof localStorage !== 'undefined') {
-    localStorage.setItem('key', 'value');
-  } else if (typeof sessionStorage !== 'undefined') {
-    // Fallback to sessionStorage if localStorage is not supported
-    sessionStorage.setItem('key', 'value');
-  } else {
-    // If neither localStorage nor sessionStorage is supported
-    console.log('Web Storage is not supported in this environment.');
-  }
-  console.log(localStorage);
+  // // localStorage.setItem('username', 'Anisha');
+  // // localStorage.setItem('userId', '12345');
+  // if (typeof localStorage !== 'undefined') {
+  //   localStorage.setItem('key', 'value');
+  // } else if (typeof sessionStorage !== 'undefined') {
+  //   // Fallback to sessionStorage if localStorage is not supported
+  //   sessionStorage.setItem('key', 'value');
+  // } else {
+  //   // If neither localStorage nor sessionStorage is supported
+  //   console.log('Web Storage is not supported in this environment.');
+  // }
+  // console.log(localStorage);
   return (
-    "rip"
+    <Box
+    display={"flex"}
+    p={4}
+   >Testing</Box>
   )
 }
